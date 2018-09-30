7b73e3d43da9209c6e0c5f98b32f684a206c38cd
Ihar Dubadzelau <igor@dubodeloff.xyz>
2018-09-30 18:04:36 -0400

    HW0 - two methods through cycle and stream

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -7,10 +7,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class UserMealsUtil {
@@ -23,10 +20,27 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
+
+        System.out.println("------------------ Speed of execution of methods is 10,000 times ----------------------------------");
+        Date startDate = new Date();
+        for (int i = 0; i < 10000; i++)
+            getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+        System.out.println("Execution time (Stream) = "+((new Date()).getTime()-startDate.getTime()));
+
+        startDate = new Date();
+        for (int i = 0; i < 10000; i++)
+            getFilteredWithExceeded_Cycle(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+        System.out.println("Execution time (Cyrcle) = "+((new Date()).getTime()-startDate.getTime()));
+
+        System.out.println("------------------ Result of the method based on the STREAM ----------------------------------");
         getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000)
                 .stream()
                 .forEach(System.out::println);
 
+        System.out.println("------------------ Result of the method based on the CYCLES ----------------------------------");
+        getFilteredWithExceeded_Cycle(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000)
+                .stream()
+                .forEach(System.out::println);
 //        .toLocalDate();
 //        .toLocalTime();
     }
@@ -37,6 +51,38 @@ public class UserMealsUtil {
         if ( mealList !=null ) {
             Map<LocalDate, Integer> mapDateCalories = mealList.stream()
                     .collect(Collectors.groupingBy( UserMeal::getDT_toLocalDate, Collectors.summingInt(UserMeal::getCalories)));
+
+            listUMWE = mealList.stream()
+                    .filter(um -> um.isBetweenTime(startTime,endTime))
+                    .map(um -> new UserMealWithExceed(um, ( mapDateCalories.getOrDefault(um.getDT_toLocalDate(),Integer.MIN_VALUE)>caloriesPerDay )))
+                    .collect(Collectors.toList());
+        }
+
+        return listUMWE;
+    }
+
+    public static List<UserMealWithExceed>  getFilteredWithExceeded_Cycle(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        List<UserMealWithExceed> listUMWE = new ArrayList<>();
+
+        if ( mealList !=null ) {
+            Map<LocalDate, Integer> mapDateCalories = new HashMap<>();
+
+            for (int i=0; i<mealList.size(); i++) {
+                UserMeal userMeal = mealList.get(i);
+                if (userMeal!=null && userMeal.isBetweenTime(startTime, endTime) ) {
+                        LocalDate localDate = userMeal.getDT_toLocalDate();
+                        Integer calories = mapDateCalories.get(localDate);
+                        if (calories == null) {
+                            calories = userMeal.getCalories();
+                            for (int j = i+1; j <mealList.size(); j++) {
+                                UserMeal userMealAdd = mealList.get(j);
+                                if ( userMealAdd!=null && userMealAdd.getDT_toLocalDate().compareTo(localDate)==0 )
+                                    calories += userMealAdd.getCalories();
+                            }
+                            mapDateCalories.put(userMeal.getDT_toLocalDate(),calories);
+                        }
+                }
+            }
 
             for (UserMeal userMeal:mealList) {
                 if ( userMeal.isBetweenTime(startTime,endTime) ) {
